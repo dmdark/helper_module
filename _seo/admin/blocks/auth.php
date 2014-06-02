04d8dbab05ed80684acfc29cd244fe51a086b443
Vadiok <sitereg>
2014-06-02 12:31:31 +0400

    Изменние верстки

--- _seo/admin/blocks/auth.php
+++ _seo/admin/blocks/auth.php
@@ -1,9 +1,15 @@
 <!-- Авторизация -->
-   <div class="auth_container">
-      <form class="form-inline" action="" method="post">
-         <input type="text" class="form-control" placeholder="Login" name="login"/>
-         <input type="password" class="form-control" placeholder="Password" name="password"/>
-         <br/>
-         <button type="submit" class="btn btn-default">Войти</button>
-      </form>
-   </div>
+<div class="container">
+	<div class="row">
+		<form class="col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3" action="" method="post">
+			<h2>Вход в панель</h2>
+			<div class="form-group">
+				<input type="text" class="form-control" placeholder="Login" name="login" autofocus/>
+			</div>
+			<div class="form-group">
+				<input type="password" class="form-control" placeholder="Password" name="password"/>
+			</div>
+			<button type="submit" class="btn btn-default">Войти</button>
+		</form>
+	</div>
+</div>
